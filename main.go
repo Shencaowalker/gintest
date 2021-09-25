0e485764a0c865f72bfc1d78bca9e0d3c85acf95
Shencaowalker <s1179114797@outlook.com>
2021-09-26 01:14:56 +0800

    项目拆分

--- main.go
+++ main.go
@@ -9,9 +9,13 @@ import (
 
 
 func main() {
+	//初始化数据库 创建表
 	db := common.InitDB()
+	//完事儿后关闭数据库
 	defer db.Close()
+	//创建gin默认实例
 	r := gin.Default()
+	//加上路由选择
 	r=CollectRoute(r)
 	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
