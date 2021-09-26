d79c49ac3811365446eeca509b6465f613fc9bcd
Shencaowalker <s1179114797@outlook.com>
2021-09-26 21:17:27 +0800

    update

--- common/database.go
+++ common/database.go
@@ -11,29 +11,40 @@ var DB *gorm.DB
 
 //初始化数据库链接
 func InitDB() *gorm.DB {
+	/*
+		driverName := viper.GetString("datasource.driverName")
+		host := viper.GetString("datasource.host")
+		post := viper.GetString("datasource.port")
+		database := viper.GetString("datasource.database")
+		username := viper.GetString("datasource.username")
+		password := viper.GetString("datasource.password")
+		charset := viper.GetString("datasource.charset")
+	*/
 	driverName := "mysql"
 	host := "116.62.114.212"
-	post := "3306"
+	port := "3306"
 	database := "gintest1"
 	username := "root"
 	password := "123456"
 	charset := "utf8mb4"
+
 	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
 		username,
 		password,
 		host,
-		post,
+		port,
 		database,
 		charset)
 	db, err := gorm.Open(driverName, args)
 	if err != nil {
 		panic("failed to connect database,err: " + err.Error())
 	}
-	//创建数据库迁移  这里主要是user表 
+	//创建数据库迁移  这里主要是user表
 	db.AutoMigrate(&model.User{})
 	DB = db
 	return db
 }
+
 //得到数据库链接
 func GetDB() *gorm.DB {
 	return DB
--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	//"github.com/spf13/viper"
 )
 
 func main() {
+	//InitConfig()
 	//初始化数据库 创建表
 	db := common.InitDB()
 	//完事儿后关闭数据库
@@ -16,5 +18,20 @@ func main() {
 	r := gin.Default()
 	//加上路由选择
 	r = CollectRoute(r)
+
 	panic(r.Run()) // 监听并在 0.0.0.0:8080 上启动服务
 }
+
+//下面代码本意读取config目录下的yml   现在不生效
+/*
+func InitConfig() {
+	v := viper.New()
+	v.SetConfigName("application")
+	v.SetConfigType("yml")
+	v.AddConfigPath("./config")
+	err := v.ReadInConfig()
+	if err != nil {
+		return
+	}
+}
+*/
