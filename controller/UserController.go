aaa6ccf39936a574c86b956689b00f52e4d921b7
Shencaowalker <s1179114797@outlook.com>
2021-09-26 09:14:57 +0800

    user login

--- controller/UserController.go
+++ controller/UserController.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Register(c *gin.Context) {
@@ -36,7 +37,6 @@ func Register(c *gin.Context) {
 		name = util.RandomString(10)
 	}
 	log.Println(name, telephone, password)
-
 	if isTelephoneExist(DB, telephone) {
 		c.JSON(http.StatusUnprocessableEntity, gin.H{
 			"code": 422,
@@ -44,20 +44,74 @@ func Register(c *gin.Context) {
 		})
 		return
 	}
-
+	//创建用户
+	hasedPassword,err := bcrypt.GenerateFromPassword([]byte(password),bcrypt.DefaultCost)
+	if err != nil{
+		c.JSON(http.StatusInternalServerError,gin.H{
+			"code": 500,
+			"msg":  "加密错误",
+		})
+		return
+	}
 	newUser := model.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hasedPassword),
 	}
 	DB.Create(&newUser)
-
 	c.JSON(200, gin.H{
+		"code":200,
 		"message": "成功注册",
 	})
 }
 
-
+func Login(c *gin.Context) {
+	DB := common.GetDB()
+	//获取参数
+	telephone := c.PostForm("telephone")
+	password := c.PostForm("password")
+	//验证数据
+	if len(telephone) != 11 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "手机号必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "密码不能少于6位",
+		})
+		return
+	}
+	//判断手机号是否存在
+	var user model.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID ==0{
+		c.JSON(http.StatusUnprocessableEntity,gin.H{
+			"code": 422,
+			"msg":  "手机号不存在",
+		})
+		return
+	}
+	//密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password),[]byte(password));err != nil{
+		c.JSON(http.StatusBadRequest,gin.H{
+			"code":400,
+			"msg":"密码错误",
+		})
+		return
+	}
+	//发放token
+	token := "1111"
+	//发放结果
+	c.JSON(200,gin.H{
+		"code":200,
+		"data":gin.H{"token":token},
+		"msg":"登陆成功",
+	})
+}
 
 func isTelephoneExist(db *gorm.DB, telephone string) bool {
 	var user model.User
